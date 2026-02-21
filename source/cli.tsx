84cb9c8c3f38904341d23b4531da864699994156
Gazi2050 <gazi45344@gmail.com>
2026-02-22 03:54:01 +0600

    docs(cli): add help options for create, update, delete, read

--- source/cli.tsx
+++ source/cli.tsx
@@ -8,6 +8,16 @@ meow(
 	`
 	Usage
 	  $ note-cli
+
+	Options
+		--create, -c  Create a new note
+		--update, -u  Update an existing note
+		--delete, -d  Delete a note
+		--read,   -r  Read/List notes
+
+	Examples
+	  $ note-cli --create
+	  $ note-cli -c
 	`,
 	{
 		importMeta: import.meta,
